d6014faf1cd109f89838d2e066c527233204a683
wanglinhai <wanglinhai@ejiayou.com>
2022-09-02 09:48:13 +0800

    OOM

---
+++ app/src/com/kk/oom/HeapOOM.java
@@ -0,0 +1,23 @@
+package com.kk.oom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ *
+ * @author k 2022/9/1 13:44
+ */
+public class HeapOOM {
+
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+
+}

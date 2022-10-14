18e596192b76865b8cc282c3c66fe96952ef76d1
wanglinhai <wanglinhai@ejiayou.com>
2022-10-14 09:16:04 +0800

    aqs原理

--- threadapp/src/com/keke/demo5/Mutex.java
+++ threadapp/src/com/keke/demo5/Mutex.java
@@ -6,13 +6,20 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
 /**
+ * 独占锁Mutex是一个自定义同步组件，它在同一时刻只允许一个线程占有
+ * 锁。Mutex中定义了一个静态内部类，该内部类继承了同步器并实现了独占式获取和释放同步
+ * 状态。在tryAcquire(int acquires)方法中，如果经过CAS设置成功（同步状态设置为1），则代表获
+ * 取了同步状态，而在tryRelease(int releases)方法中只是将同步状态重置为0。用户使用Mutex时
+ * 并不会直接和内部同步器的实现打交道，而是调用Mutex提供的方法，在Mutex的实现中，以获
+ * 取锁的lock()方法为例，只需要在方法实现中调用同步器的模板方法acquire(int args)即可，当
+ * 前线程调用该方法获取同步状态失败后会被加入到同步队列中等待，这样就大大降低了实现
+ * 一个可靠自定义同步组件的门槛。
  * Lock api
  *
  * @author k 2022/9/28 22:56
  */
-public class Mutex implements Lock {
-
-    // 自定义同步器
+class Mutex implements Lock {
+    // 静态内部类，自定义同步器
     private static class Sync extends AbstractQueuedSynchronizer {
         // 是否处于占用状态
         @Override
@@ -23,6 +30,24 @@ public class Mutex implements Lock {
         // 当状态为0的时候获取锁
         @Override
         public boolean tryAcquire(int acquires) {
+//            /** Marker to indicate a node is waiting in shared mode */
+//            static final Node SHARED = new Node();
+//            /** Marker to indicate a node is waiting in exclusive mode */
+//            static final Node EXCLUSIVE = null;
+//
+//            /** waitStatus value to indicate thread has cancelled */
+//            static final int CANCELLED =  1;
+//            /** waitStatus value to indicate successor's thread needs unparking */
+//            static final int SIGNAL    = -1;
+//            /** waitStatus value to indicate thread is waiting on condition */
+//            static final int CONDITION = -2;
+//            /**
+//             * waitStatus value to indicate the next acquireShared should
+//             * unconditionally propagate
+//             */
+//            static final int PROPAGATE = -3;
+
+
             if (compareAndSetState(0, 1)) {
                 setExclusiveOwnerThread(Thread.currentThread());
                 return true;
@@ -34,7 +59,8 @@ public class Mutex implements Lock {
         @Override
         protected boolean tryRelease(int releases) {
             if (getState() == 0) {
-                throw new IllegalMonitorStateException();
+                throw new
+                        IllegalMonitorStateException();
             }
             setExclusiveOwnerThread(null);
             setState(0);
@@ -47,6 +73,7 @@ public class Mutex implements Lock {
         }
     }
 
+    // 仅需要将操作代理到Sync上即可
     private final Sync sync = new Sync();
 
     @Override
@@ -55,18 +82,8 @@ public class Mutex implements Lock {
     }
 
     @Override
-    public void lockInterruptibly() throws InterruptedException {
-        sync.acquireInterruptibly(1);
-    }
-
-    @Override
     public boolean tryLock() {
         return sync.tryAcquire(1);
-    }
-
-    @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return sync.tryAcquireNanos(1, unit.toNanos(time));
     }
 
     @Override
@@ -77,5 +94,23 @@ public class Mutex implements Lock {
     @Override
     public Condition newCondition() {
         return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(timeout));
     }
 }

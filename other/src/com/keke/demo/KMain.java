836e0c7c8e4b8c012c09fea77308c13fa6bcb3aa
wanglinhai <wanglinhai@ejiayou.com>
2022-11-29 10:31:45 +0800

    LocalDateTime 工具类

---
+++ other/src/com/keke/demo/KMain.java
@@ -0,0 +1,29 @@
+package com.keke.demo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * LocalDateTime 工具类学习
+ * @author k 2022/11/28 15:17
+ */
+public class KMain {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String format = dtf.format(LocalDateTime.now());
+        System.out.println(format);
+
+        LocalDateTime parse = LocalDateTime.parse(format,dtf);
+        System.out.println(parse);
+        int dayOfMonth = parse.getDayOfMonth();
+        int monthValue = parse.getMonthValue();
+        LocalDateTime minusDays = parse.minusDays(1);
+        LocalDateTime minusHours = parse.minusHours(2);
+        LocalDateTime localDateTime = parse.withDayOfYear(200);
+        System.out.println("dayOfMonth: " + dayOfMonth );
+        System.out.println("monthValue: " + monthValue );
+        System.out.println("minusDays: " + minusDays );
+        System.out.println("minusHours: " + minusHours );
+        System.out.println("localDateTime: " + localDateTime );
+    }
+}
